165b025b67e3b300b1ecee3053abd07a4ed254d5
Weslei Almeida Muniz <88255036+WesleiAlmeidaMuniz@users.noreply.github.com>
2024-04-07 15:42:21 -0300

    Exercicio finalizado

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'DevChuva';
+  myForm: FormGroup;
+  showMessage: boolean = false;
+  mostrarFormulario: boolean = false;
+  mostrarTopico: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      assunto: ['', Validators.required],
+      conteudo: ['', [Validators.required]]
+    });
+  }
+
+  submitForm() {
+    this.showMessage = true;
+    this.mostrarFormulario = false;
+  }
+
+  toggleFormulario() {
+    this.mostrarFormulario = !this.mostrarFormulario;
+    this.showMessage = false; 
+  }
+
+  toggleTopico(){
+    this.mostrarTopico = !this.mostrarTopico;
+  }
 }
